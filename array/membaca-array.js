25196df59cb10a3cd0f8bc27fc4da6d065b33a7b
prabowo-sakti <prabowosakti2023@gmail.com>
2025-01-12 15:34:53 +0700

    mempelajari struktur data array

---
+++ array/membaca-array.js
@@ -0,0 +1,6 @@
+const arr = [1,2,3,5] //Menginisialisasikan variabel arr untuk array angka
+const value = arr[3] //membaca elemen ti index ke 3
+console.log(value) //output: 5
+
+
+// Konklusi, membaca array dengan index, seperti contoh diatas, kompleksitas waktu big o yang dibutuhkan adalah O(1)
---
+++ array/mencari-nilai-yang-tidak-ada-di-array.js
@@ -0,0 +1,8 @@
+const array = Array.from({length: 1000}, (_, i) => i + 1)
+
+const target = 200
+
+let found = false
+for(let i = 0; i < array.length; i++) {
+    
+}
